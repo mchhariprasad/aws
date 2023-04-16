173fab78f67c0883f139858fcd9a3a1358e16cf9
Thriveni <Thriveni@LAPTOP-5RFAD3LM>
2023-04-16 12:27:47 +0530

    test1

---
+++ src/main/java/com/aws/Aws1Application.java
@@ -0,0 +1,13 @@
+package com.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aws1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aws1Application.class, args);
+	}
+
+}
