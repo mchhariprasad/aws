a127bfc3388d5c5eafcca1accfd6cd6f00d4d7d6
mchhariprasad <mchhariprasad@gmail.com>
2023-04-16 13:34:39 +0530

    rrrrr

---
+++ src/main/java/com/aws/RestControllerEx.java
@@ -0,0 +1,23 @@
+package com.aws;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aws")
+public class RestControllerEx {
+
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		return ResponseEntity.ok("Hello World!! GET test");
+	}
+
+	@PostMapping("/test-post")
+	public String testPost() {
+		return "Success from test POST";
+	}
+
+}
